1b7aa6ec645aa22e891fd4a3999e606135df43c8
Gwan <hankwan777>
2022-05-17 18:55:38 +0900

    JPA를 곁들인 Posts 저장 Controller, Service, Dto, Test 생성 TestRestTemplate 를 사용한 SpringWebMVC + JPA 테스트

---
+++ src/main/java/com/donkey/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.donkey.book.springboot.service.posts;
+
+import com.donkey.book.springboot.domain.posts.PostsRepository;
+import com.donkey.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
