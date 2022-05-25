fda560ecd252b88d3805e68648c63925d42d378e
hankw <hankwan777@gmail.com>
2022-05-25 21:55:06 +0900

    게시글 등록 후 index 에서 전체조회 기능 추가

--- src/main/java/com/donkey/book/springboot/web/IndexController.java
+++ src/main/java/com/donkey/book/springboot/web/IndexController.java
@@ -1,13 +1,20 @@
 package com.donkey.book.springboot.web;
 
+import com.donkey.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
@@ -15,5 +22,4 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
-
 }
