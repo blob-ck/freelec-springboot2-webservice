129c224107c3f2fb899658b4a91d3ecccc9691a4
Gwan <hankwan777>
2022-06-21 13:33:35 +0900

    [test]application.properties 에 spring.profiles.include=real-oauth 추가해서 시도

--- scripts/start.sh
+++ scripts/start.sh
@@ -31,6 +31,6 @@ echo "nohup java -jar \
           -Dspring.profiles.active=$IDLE_PROFILE \
           $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &"
 nohup java -jar \
-    -Dspring.config.location=classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
