668de2a21ad1cb57d2bd5100071d0dd6bafbc61b
Gwan <hankwan777>
2022-06-21 10:38:18 +0900

    appspec 내부 훅 스크립트 변경 profile crul 오타 수정 -> curl

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -3,7 +3,7 @@
 # 쉬고 있는 profile 찾기: real1 or real2
 
 function find_idle_profile() {
-  RESPONSE_CODE=$(crul -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
   if [ ${RESPONSE_CODE} -ge 400 ] # status code 가 400 보다 크면(즉 4xx, 5xx 에러 발생시)
   then
